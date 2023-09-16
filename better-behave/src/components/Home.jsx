f16b40af4baa9c546392c6f34b39add738a347d6
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2023-09-16 02:39:40 -0400

    home page added

--- better-behave/src/components/Home.jsx
+++ better-behave/src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome User!</h1>
+      <h3>Practiced: 10</h3>
+      <h3>Mastered: 4</h3>
+      <button>
+      <NavLink to="/question" className="w-28 h-8 mx-12 inline-block mt-24 bg-gray-800 text-white rounded-xl">
+        New Interview Practice
+      </NavLink>
+      </button>
     </div>
   )
 }
