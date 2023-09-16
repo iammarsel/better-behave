9df4b1898bda6cc0d5290d976afbdf25d2600a7e
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2023-09-16 00:59:23 -0400

    react router works

---
+++ better-behave/src/components/SignIn.jsx
@@ -0,0 +1,9 @@
+function Signin() {
+  return (
+    <div>
+      <h1>SignIn</h1>
+    </div>
+  )
+}
+
+export default Signin
---
+++ better-behave/src/components/SignUp.jsx
@@ -0,0 +1,9 @@
+function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up here</h1>
+    </div>
+  )
+}
+
+export default SignUp
