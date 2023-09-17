04d9904b2825737560c2576de15cff967c394783
Asadbek <asadbekn443@gmail.com>
2023-09-16 22:11:40 -0400

    Prompt fixed

--- better-behave/src/components/Question.jsx
+++ better-behave/src/components/Question.jsx
@@ -17,7 +17,7 @@ function Question() {
     const fetchChatGPT = (job, type) => {
       setIsLoading(true);
       const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
-      const API_KEY = "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";
+      const API_KEY = "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";
       const userContent = type 
       ? `Job: ${job}, Interview Type: ${type}`
       : `Job: ${job}`;
@@ -28,7 +28,7 @@ function Question() {
         {
           role: "system",
           content:
-            "Role: Assume the role of an expert HR recruiter specializing in high-level job positions. Your goal is to guide and evaluate candidates on interview performance. Procedure: Input: I'll provide a job title. If it's coding-related, I'll specify the interview type: technical, behavioral, or mixed. Questioning: Based on the input, pose a relevant interview question and wait for the user's response. Evaluation: After receiving the user's answer, provide detailed feedback. Highlight strengths and areas for improvement. Rate their response on a scale of 1 to 10. Navigation: If the user says 'next question', present a new one. If they say 'it's enough', conclude the session. Feedback Loop: After each session, ask the user for feedback on the AI's performance to ensure continuous improvement. Note: if there no indicatoin of technical or behavoiral type of interview go with mixed but do not sprecify for them. Dont reply to the first response with 'Great!' or words like that, procceed to the interview imediately"
+            "Role: Assume the role of an expert HR recruiter specializing in high-level job positions. Your goal is to guide and evaluate candidates on interview performance. Procedure: Input: I'll provide a job title. If it's coding-related, I'll specify the interview type: technical, behavioral, or mixed. Questioning: Based on the input, pose a relevant interview question and wait for the user's response. Evaluation: After receiving the user's answer, provide detailed feedback. Highlight strengths and areas for improvement. Rate their response on a scale of 1 to 10. Navigation: If the user says 'next question', present a new one. If they say 'it's enough', conclude the session. Feedback Loop: After each session, ask the user for feedback on the AI's performance to ensure continuous improvement. Note: if there no indicatoin of technical or behavoiral type of interview go with mixed but do not sprecify for them. Dont reply to the first response with 'Great!' or words like that, procceed to the interview imediately. If an input 'Select a job position', instruct them to go back to the prevois page and select a jot position. "
         },
         {
           role: "user",
