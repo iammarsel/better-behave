20f9e8f9acb6e631fd60188f8cefe88a1ee68c4a
Pavan Kumar Mathamsetty <pavankumarm2505@gmail.com>
2023-09-17 10:32:12 -0400

    made taipy work

--- better-behave/server/taipy_charts.py
+++ better-behave/server/taipy_charts.py
@@ -2,37 +2,35 @@
 
 from flask import Flask, send_file 
 from taipy import Gui
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+import random
 
-cred = credentials.Certificate('shellhacks-7e288-firebase-adminsdk-vyxsp-edcfb6bb52.json')
+# cred = credentials.Certificate('shellhacks-7e288-firebase-adminsdk-vyxsp-edcfb6bb52.json')
 
 
 
-# Application Default credentials are automatically created.
-ref = firebase_admin.initialize_app(cred, {
-  'databaseURL': 'https://shellhacks.firebaseio.com'
-})
-print(firebase_admin.get_app())
-seshs_ref = db.reference()
-print(seshs_ref)
-seshs1 = seshs_ref.child('sessions')
-seshs =  seshs1.get()
-# data = {
-#   'Date': list(range(10)),
-#   'Score': [s.score for s in seshs]
-# }
-dates = []
-scores = []
+# # Application Default credentials are automatically created.
+# ref = firebase_admin.initialize_app(cred, {
+#   'databaseURL': 'https://shellhacks.firebaseio.com'
+# })
+# print(firebase_admin.get_app())
+# seshs_ref = db.reference()
+# print(seshs_ref)
+# seshs1 = seshs_ref.child('sessions')
+# seshs =  seshs1.get()
+data = {
+  'x': list(range(10)),
+  'y': [random.randint(0, 10) for _ in range(10)]
+}
+# dates = []
+# scores = []
 
-for x, y in seshs.items():
-    dates.append(y.get('completedAt', ''))  # Get the 'date' field from the session
-    scores.append(y.get('score', '')) 
+# for x, y in seshs.items():
+#     dates.append(y.get('completedAt', ''))  # Get the 'date' field from the session
+#     scores.append(y.get('score', '')) 
     
     
-print(dates)
-print(scores)
+# print(dates)
+# print(scores)
 app = Flask(__name__) 
 
 # Generate chart
