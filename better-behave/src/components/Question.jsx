014ed425381ab5b30b89beed824b7a09128b593e
Asadbek <asadbekn443@gmail.com>
2023-09-16 22:30:03 -0400

    Chat Style Responses

--- better-behave/src/components/Question.jsx
+++ better-behave/src/components/Question.jsx
@@ -1,34 +1,35 @@
 import React, { useState, useEffect, useContext } from "react";
 import JobContext from "./JobContext";
 import "./loading.css";
-import AudioRecorderButton from './AudioRecorderButton';
+import AudioRecorderButton from "./AudioRecorderButton";
 
 function Question() {
-    const { selectedJob, selectedType } = useContext(JobContext);
-  
-    const [isLoading, setIsLoading] = useState(false);
-    const [responseMessage, setResponseMessage] = useState(null);
+  const { selectedJob, selectedType } = useContext(JobContext);
+  const [messages, setMessages] = useState([]);
 
-    useEffect(() => {
-        // Fetch the API immediately upon loading
-        fetchChatGPT(selectedJob, selectedType);
-    }, []);
-  
-    const fetchChatGPT = (job, type) => {
-      setIsLoading(true);
-      const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
-      const API_KEY = "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";
-      const userContent = type 
+  const [isLoading, setIsLoading] = useState(false);
+  const [responseMessage, setResponseMessage] = useState(null);
+
+  useEffect(() => {
+    // Fetch the API immediately upon loading
+    fetchChatGPT(selectedJob, selectedType);
+  }, []);
+
+  const fetchChatGPT = (job, type) => {
+    setIsLoading(true);
+    const API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
+    const API_KEY = "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";
+    const userContent = type
       ? `Job: ${job}, Interview Type: ${type}`
       : `Job: ${job}`;
 
-      const data = {
+    const data = {
       model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
           content:
-            "Role: Assume the role of an expert HR recruiter specializing in high-level job positions. Your goal is to guide and evaluate candidates on interview performance. Procedure: Input: I'll provide a job title. If it's coding-related, I'll specify the interview type: technical, behavioral, or mixed. Questioning: Based on the input, pose a relevant interview question and wait for the user's response. Evaluation: After receiving the user's answer, provide detailed feedback. Highlight strengths and areas for improvement. Rate their response on a scale of 1 to 10. Navigation: If the user says 'next question', present a new one. If they say 'it's enough', conclude the session. Feedback Loop: After each session, ask the user for feedback on the AI's performance to ensure continuous improvement. Note: if there no indicatoin of technical or behavoiral type of interview go with mixed but do not sprecify for them. Dont reply to the first response with 'Great!' or words like that, procceed to the interview imediately. If an input 'Select a job position', instruct them to go back to the prevois page and select a jot position. "
+            "Role: Assume the role of an expert HR recruiter specializing in high-level job positions. Your goal is to guide and evaluate candidates on interview performance. Procedure: Input: I'll provide a job title. If it's coding-related, I'll specify the interview type: technical, behavioral, or mixed. Questioning: Based on the input, pose a relevant interview question and wait for the user's response. Evaluation: After receiving the user's answer, provide detailed feedback. Highlight strengths and areas for improvement. Rate their response on a scale of 1 to 10. Navigation: If the user says 'next question', present a new one. If they say 'it's enough', conclude the session. Feedback Loop: After each session, ask the user for feedback on the AI's performance to ensure continuous improvement. Note: if there no indicatoin of technical or behavoiral type of interview go with mixed but do not sprecify for them. Dont reply to the first response with 'Great!' or words like that, procceed to the interview imediately. If an input 'Select a job position', instruct them to go back to the prevois page and select a jot position. ",
         },
         {
           role: "user",
@@ -47,8 +48,8 @@ function Question() {
     })
       .then((response) => response.json())
       .then((data) => {
-        const message = data.choices[0].message.content;
-        setResponseMessage(message); // Update the state with the response
+        const newMessage = data.choices[0].message.content;
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -61,14 +62,16 @@ function Question() {
       <h1 className="">Question 1: Tell me about Yourself</h1>
       <h1>Selected Job: {selectedJob}</h1>
       <h2>Selected Interview Type: {selectedType}</h2>
-      {responseMessage && (
-        <div
-          id="responseBox"
-          className="border p-4 rounded-3xl rounded-bl-none bg-blue-500 shadow-sm"
-        >
-          {responseMessage}
-        </div>
-      )}
+      <div className="messages-container">
+        {messages.map((message, index) => (
+          <div
+            key={index}
+            className="border p-4 rounded-3xl rounded-bl-none bg-blue-500 shadow-sm mb-2"
+          >
+            {message}
+          </div>
+        ))}
+      </div>
       {isLoading && (
         <div className="center">
           <div className="wave"></div>
@@ -84,10 +87,9 @@ function Question() {
         </div>
       )}{" "}
       {/* Display loading indicator based on state */}
-      <button onClick={() => fetchChatGPT("blue")}>Blue</button>
+      <button onClick={() => fetchChatGPT("next question")}>Blue</button>
       <button onClick={() => fetchChatGPT("red")}>Red</button>
       <AudioRecorderButton />
-
     </div>
   );
 }
