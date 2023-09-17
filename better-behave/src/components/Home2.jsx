7c176bad4e7ad66b39055e0de991a1b2d5a4ff1a
Asadbek <asadbekn443@gmail.com>
2023-09-17 10:57:17 -0400

    Last Commit: Home page

--- better-behave/src/components/Home2.jsx
+++ better-behave/src/components/Home2.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom'; // Updated import
 import { auth, db } from '../firebase';
+import logo from "../assets/logo_gimmejob.jpeg";
+
 
 function Home2() {
   console.log("Home")
@@ -14,6 +16,19 @@ function Home2() {
     } catch (error) {
       console.error('Error signing out:', error.message);
     }
+  };
+  const buttonStyle = {
+    backgroundColor: "#FECC57",
+    color: "#233068",
+    padding: "8px 16px",
+    marginTop: '30px',
+    borderRadius: "8px",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",
+    width: "100%", // Make the button take full width
+    padding: "12px", // Increase padding for better spacing
+    fontSize: "1.2em", // Increase font size
+    fontFamily: "Arial, sans-serif", // Change font
   };
 
   db.collection('sessions').doc('session_template').get().then((data) => {
@@ -51,22 +66,35 @@ function Home2() {
   })
 
 
-  return (
-    <div>
-      <h1>Welcome User!</h1>
-      <h3>Practiced: {completed}</h3>
-      <h3>Mastered: {mastered}</h3>
-      <iframe src='http://localhost:5000/' style={iframeStyle} title="Chart" /> 
-
-      <button onClick={handleSignOut}>Sign Out</button>
-      <button>
-      <NavLink to="/question">
-        New Interview Practice
-      </NavLink>
+ return (
+    <div className="bg-1A244D min-h-screen flex flex-col items-center justify-center text-FECC57">
+      <img
+        src={logo} // Replace with the URL of your logo image
+        alt="Logo"
+        style={{
+          position: "absolute",
+          top: "20px", // Adjust the top position as needed
+          left: "20px", // Adjust the left position as needed
+          width: "250px", // Adjust the width of the logo as needed
+        }}/>
+      <h1 className="text-4xl mb-6">Welcome User!</h1>
+      <button 
+        onClick={handleSignOut}  
+        style={buttonStyle}
+        className="bg-FECC57 text-1A244D px-6 py-2 rounded-lg mb-4 hover:bg-opacity-80 transition duration-300 ease-in-out"
+        
+      >
+        Sign Out
+      </button>
+      <button 
+        style={buttonStyle} className="bg-FECC57 text-1A244D px-6 py-2 rounded-lg hover:bg-opacity-80 transition duration-300 ease-in-out">
+        <NavLink to="/question" className="text-1A244D">
+          New Interview Practice
+        </NavLink>
       </button>
     </div>
-  );
-}
+);
+ }
 
 export default Home2;
 
